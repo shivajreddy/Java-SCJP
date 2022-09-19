86b026596ac8532e0064538d1175df8f315818bd
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-09-19 16:43:28 -0400

    .

--- com.JavaSCJP/src/Operators.java
+++ com.JavaSCJP/src/Operators.java
@@ -1,9 +1,44 @@
-public class Operators {
+public class Operators{
+  public static void main(String[] args) {
 
-  final int num = 10;
+    Person p = new Person();
+    Vehicle v = new Vehicle();
+    Computer c = new Computer();
 
-  public static void meth(){
-    System.out.println("Method inside");
+//    System.out.println(p instanceof Vehicle);
+
+    Object[] arr = {p, v, c};
+
+    Object o1 = arr[0];
+    Object o2 = arr[1];
+    Object o3 = arr[2];
+
+    System.out.println(o1 instanceof Vehicle);
+
+    if (o1 instanceof Person){
+      System.out.println("is of type Person");
+    } else if (o1 instanceof Vehicle) {
+      System.out.println("is of type Vehicle");
+    } else if (o1 instanceof Computer) {
+      System.out.println("is of type Computer");
+    } else {
+      System.out.println("Just an plain object");
+    }
+
   }
+
+
+}
+
+
+class Person{
+
+}
+
+class Vehicle{
+
+}
+
+class Computer{
 
 }
