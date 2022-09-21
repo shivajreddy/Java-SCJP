f64f392ac419ae4a7450ddc8aae15af83f985277
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-09-20 22:40:54 -0400

    finished 23 - operators

--- com.JavaSCJP/src/Operators.java
+++ com.JavaSCJP/src/Operators.java
@@ -1,44 +1,17 @@
 public class Operators{
-  public static void main(String[] args) {
 
-    Person p = new Person();
-    Vehicle v = new Vehicle();
-    Computer c = new Computer();
+  public static void main(String[] anything){
 
-//    System.out.println(p instanceof Vehicle);
+    System.out.println(m1(1) + m1(2) * m1(3) / m1(4) + m1(5) * m1(6));
 
-    Object[] arr = {p, v, c};
-
-    Object o1 = arr[0];
-    Object o2 = arr[1];
-    Object o3 = arr[2];
-
-    System.out.println(o1 instanceof Vehicle);
-
-    if (o1 instanceof Person){
-      System.out.println("is of type Person");
-    } else if (o1 instanceof Vehicle) {
-      System.out.println("is of type Vehicle");
-    } else if (o1 instanceof Computer) {
-      System.out.println("is of type Computer");
-    } else {
-      System.out.println("Just an plain object");
-    }
 
   }
 
+  private static int m1(int i){
+    System.out.println(i);
+    return i;
+  }
 
-}
 
-
-class Person{
-
-}
-
-class Vehicle{
-
-}
-
-class Computer{
 
 }
