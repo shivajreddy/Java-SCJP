d182093a0707fde9558b5d60c9d865150d1449f8
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-09-30 12:03:01 -0400

    finished modifiers completely. moving on to OOP

--- com.JavaSCJP/src/Test.java
+++ com.JavaSCJP/src/Test.java
@@ -1,6 +1,8 @@
-public class Test {
+public class Test
+{
 
-  public static void main(String[] args){
+  public static void main(String[] args)
+  {
 
     Student s = new Student("shiva", 27, 21, 80);
   }
