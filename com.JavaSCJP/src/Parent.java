00ff90dc6c919faae58093c54b9d1a94e5faaf46
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-10-04 15:55:14 -0400

    .

--- com.JavaSCJP/src/Parent.java
+++ com.JavaSCJP/src/Parent.java
@@ -1,9 +1,15 @@
 public class Parent
 {
+  void m1(){
+    System.out.println("parent m1");
+  }
 }
 
 
 class Child extends Parent
 {
-
+  void m1(){
+    System.out.println("child m1");
+  }
+  void m2(){}
 }
--- com.JavaSCJP/src/Test.java
+++ com.JavaSCJP/src/Test.java
@@ -1,6 +1,14 @@
 public class Test
 {
+
+  static {
+  System.out.println("hey man");
+  System.exit(0);
+  }
+
   public static void main(String[] args){
 
   }
+
 }
+
