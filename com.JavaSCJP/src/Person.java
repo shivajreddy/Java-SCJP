d0621abb1b526d29d3d4df8fc2ad76a5d4afcb3d
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-10-10 10:51:52 -0400

    started exception handling

--- com.JavaSCJP/src/Person.java
+++ com.JavaSCJP/src/Person.java
@@ -1,30 +1,4 @@
-import java.io.IOException;
-
 public class Person
 {
 
-  Person() throws IOException {
-  }
-
-
-  public static void main(String[] args) {
-
-    // Student s = new Student(10);
-
-  }
-
 }
-
-class Student extends Person
-{
-  // Student() throws IOException {}
-  // Student(){
-  //   try{
-  //   super();
-  //   }
-  //     catch (IOException e){
-  //     System.out.println(e);
-  //     }
-  // }
-}
-
--- com.JavaSCJP/src/Test.java
+++ com.JavaSCJP/src/Test.java
@@ -1,7 +1,3 @@
-abstract class Test
+public class Test
 {
-  Test(){
-
-  }
-
 }
