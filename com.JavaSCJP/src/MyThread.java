24d795255690f2544d7547fd3ac156c25c28836e
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-10-11 16:35:13 -0400

    multi-threading intro, intializing thread's

---
+++ com.JavaSCJP/src/MyThread.java
@@ -0,0 +1,11 @@
+// Defining a thread
+public class MyThread extends Thread {
+
+  @Override
+  public void run(){      // what ever is in the body of run(), it's the called 'job' of the thread
+    for (int i = 0; i < 10; i++){
+      System.out.println("child thread. task: " + i);
+    }
+  }
+
+}
--- com.JavaSCJP/src/Test.java
+++ com.JavaSCJP/src/Test.java
@@ -3,6 +3,19 @@ public class Test
   public static void main(String[] args)
   {
 
+    // at this point there is 1 thread -> main thread
+    MyThread t = new MyThread();      // This is called "Thread initialization"
+
+    t.start();      // Starting this child thread -> "Starting a Thread"
+
+    // at this point there are 2 threads -> main thread, t thread (which we just created & started)
+    // from now on, all the jobs in both the threads above will execute simultaneously. we will mixed output
+
+    for (int i = 0; i < 10; i++){
+      System.out.println("main thread. task: " + i);
+    }
+
   }
 
 }
+
