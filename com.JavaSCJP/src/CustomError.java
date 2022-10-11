2f5a8fae502ab2ac6f1eadda05a1200e4264bc2a
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-10-11 10:32:45 -0400

    custom exceptions

--- com.JavaSCJP/src/CustomError.java
+++ com.JavaSCJP/src/CustomError.java
@@ -2,10 +2,3 @@ class TooOldException extends RuntimeException
 {
   TooOldException(String errorMessage){}
 }
-
-class TooYoungException extends RuntimeException
-{
-  TooYoungException(String errorMessage){}
-
-  void m1(){}
-}
--- com.JavaSCJP/src/Test.java
+++ com.JavaSCJP/src/Test.java
@@ -2,10 +2,31 @@ public class Test
 {
   public static void main(String[] args)
   {
-    int givenAge = Integer.parseInt(args[0]);
-
-    if (givenAge > 90){
-      throw new TooYoungException("You are too young, please wait some more time, definitely you will get best match");
+    int givenAge;
+    if (args.length > 0){
+      givenAge = Integer.parseInt(args[0]);
+      if (givenAge > 90){
+        try{
+          throw new TooYoungException("You are too young");
+        } catch (TooYoungException e){
+          System.out.println(e);
+        System.out.println(e.toString());
+        System.out.println(e.getMessage());
+        }
+      }
     }
+
+  }
+}
+
+class TooYoungException extends RuntimeException
+{
+  private String errorMessage;
+  TooYoungException(String errorMessage){
+    this.errorMessage = errorMessage;
+  }
+  @Override
+  public String getMessage(){
+    return errorMessage;
   }
 }
