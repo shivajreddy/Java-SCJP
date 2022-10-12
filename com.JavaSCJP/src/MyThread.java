c326073f4cd9b2568eb60795e29d7995ebe422ce
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-10-12 15:36:15 -0400

    setting priority for threads

--- com.JavaSCJP/src/MyThread.java
+++ com.JavaSCJP/src/MyThread.java
@@ -2,7 +2,9 @@ public class MyThread extends Thread{
 
   public void run(){
     for (int i = 0; i < 10; i++){
-      System.out.println("child: " + i + " with priority: " + Thread.currentThread().getPriority());
+      System.out.println("child thread");
+      Thread.yield();
+      // System.out.println("child: " + i + " with priority: " + Thread.currentThread().getPriority());
     }
   }
 }
--- com.JavaSCJP/src/Test.java
+++ com.JavaSCJP/src/Test.java
@@ -7,7 +7,8 @@ public class Test
     t.start();
 
     for (int i = 0; i < 10; i++){
-      System.out.println("main : " + i + " with priority: " + Thread.currentThread().getPriority());
+      System.out.println("main thread");
+      // System.out.println("main : " + i + " with priority: " + Thread.currentThread().getPriority());
     }
   }
 }
