03e23e8b74737b90c04ba0e475a2f5cab86dca20
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-10-12 13:50:03 -0400

    multi-threading: run() start() overriding and overloading each. target for new threads

--- com.JavaSCJP/src/MyThread.java
+++ com.JavaSCJP/src/MyThread.java
@@ -1,12 +1,12 @@
-// Defining a thread
-public class MyThread extends Thread {
+public class MyThread extends Thread{
 
-  @Override
-  public void run(){      // what ever is in the body of run(), it's the called 'job' of the thread
-    for (int i = 0; i < 10; i++){
-      System.out.println("child thread. task: " + i);
-    }
+  public void run(){
+
+    System.out.println("by: " + Thread.currentThread().getName());
+    // for (int i = 0; i < 10; i++){
+    //   System.out.println("----- child" + i);
+    // }
+
   }
-
 }
 
