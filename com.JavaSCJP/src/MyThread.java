c6812a435164457ed714f70efa34227230e94abe
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-10-13 09:14:36 -0400

    setting priority for threads

--- com.JavaSCJP/src/MyThread.java
+++ com.JavaSCJP/src/MyThread.java
@@ -1,10 +1,20 @@
-public class MyThread extends Thread{
+public class MyThread extends Thread {
 
-  public void run(){
-    for (int i = 0; i < 10; i++){
-      System.out.println("child thread");
-      Thread.yield();
-    }
+  public void run() {
+
+    for (int i = 0; i < 10; i++)
+      {
+      System.out.println("seetha thread");
+
+      try
+        {
+        Thread.sleep(2000);
+        } catch (InterruptedException e)
+        {
+        System.out.println(e);
+        }
+
+      }
   }
 }
 
--- com.JavaSCJP/src/Test.java
+++ com.JavaSCJP/src/Test.java
@@ -1,21 +1,16 @@
-public class Test
-{
-  public static void main(String[] args) throws InterruptedException
-  {
-    // Thread main = Thread.currentThread();
+public class Test {
+  public static void main(String[] args) throws InterruptedException {
 
     MyThread t = new MyThread();
-    // MyThread t2 = new MyThread();
-    // t2.start();
-    // t.join();
-    // t.start();
 
-    t.join();
     t.start();
 
+    t.join();
 
-    for (int i = 0; i < 10; i++){
-      System.out.println("main thread");
-    }
+    for (int i = 0; i < 10; i++)
+      {
+      System.out.println("rama thread");
+      }
+
   }
 }
