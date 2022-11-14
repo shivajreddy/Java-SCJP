1c1e7d514765b23739faf87c45119210c8e5ee70
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-11-14 16:40:23 -0500

    inner classes

--- com.JavaSCJP/src/Test.java
+++ com.JavaSCJP/src/Test.java
@@ -3,12 +3,17 @@ public class Test
 
     public static void main(String[] args)
     {
-        System.out.println("hey man");
+        System.out.println("Outer class main method");
     }
 
     // Inner class
     class Inner
     {
+        public static void main(String[] args)
+        {
+            System.out.println("Inner class main method");
+        }
+
         void method1()
         {
             System.out.println("this is inner class method1");
