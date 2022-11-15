a73e9b9709bcc2eba4458be573b43533e45db851
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-11-15 11:17:28 -0500

    language fundamentals revision

--- com.JavaSCJP/src/Test.java
+++ com.JavaSCJP/src/Test.java
@@ -3,8 +3,18 @@ public class Test
     public static void main(String[] args)
     {
         // Language Fundamental revision
+        char c = 'a';
 
+        System.out.println(c);
 
+        char c1 = 100;
+        System.out.println(c1);
+
+        char c2 = '\u1000';
+        System.out.println(c2);
+
+        int i = 0b100;
+        System.out.println(i);
 
     }
 }
