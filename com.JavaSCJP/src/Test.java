92728d4797a960ca112245a785d285c0ea5f6246
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-11-15 11:38:10 -0500

    language fundamentals revision- arrays

--- com.JavaSCJP/src/Test.java
+++ com.JavaSCJP/src/Test.java
@@ -1,20 +1,10 @@
+import java.util.Arrays;
+
 public class Test
 {
     public static void main(String[] args)
     {
-        // Language Fundamental revision
-        char c = 'a';
-
-        System.out.println(c);
-
-        char c1 = 100;
-        System.out.println(c1);
-
-        char c2 = '\u1000';
-        System.out.println(c2);
-
-        int i = 0b100;
-        System.out.println(i);
-
+        int[][] arr = {{1, 2, 3}, {10, 20, 30}};
+        System.out.println(Arrays.toString(arr[0]) + " " + Arrays.toString(arr[1]));
     }
 }
