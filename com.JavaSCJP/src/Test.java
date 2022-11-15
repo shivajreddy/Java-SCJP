218f81d6627aa170e5eef7b01f90b58f38f60036
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-11-15 10:54:02 -0500

    language fundamentals revision

--- com.JavaSCJP/src/Test.java
+++ com.JavaSCJP/src/Test.java
@@ -1,23 +1,10 @@
 public class Test
 {
-
     public static void main(String[] args)
     {
-        System.out.println("Outer class main method");
+        // Language Fundamental revision
+
+
+
     }
-
-    // Inner class
-    class Inner
-    {
-        public static void main(String[] args)
-        {
-            System.out.println("Inner class main method");
-        }
-
-        void method1()
-        {
-            System.out.println("this is inner class method1");
-        }
-    }
-
 }
