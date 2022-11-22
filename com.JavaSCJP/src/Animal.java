9b2a202ea5b5a284c50776010bf4695a74510149
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-11-22 13:55:23 -0500

    final, abstract, public, private, protected; finished revision

---
+++ com.JavaSCJP/src/Animal.java
@@ -0,0 +1,20 @@
+public class Animal {
+
+    // instance variables
+    final int x;
+
+    final int x1 = 10;
+
+    // {
+    //     x = 10;
+    // }
+
+    Animal() {
+        x = 10;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
