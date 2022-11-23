8390a52c2a6214e918f5b7802195b9cf82168651
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-11-23 09:11:52 -0500

    thread priority

--- com.JavaSCJP/src/Demo.java
+++ com.JavaSCJP/src/Demo.java
@@ -2,23 +2,36 @@ public class Demo {
 
     public static void main(String[] args) throws InterruptedException {
 
-        MyRunnable r = new MyRunnable();
+        // set low priority for main thread
+        Thread mainThread = Thread.currentThread();
+        mainThread.setPriority(1);
 
+        MyRunnable r = new MyRunnable();
         Thread t = new Thread(r); // r is the target, where r is Runnable(MyRunnable extends Runnable)type
         t.start();
-        r.run();
+        t.setPriority(10);
 
+        // System.out.println("main prior = " + Thread.currentThread().getPriority());
+        // System.out.println("my thread prior = " + t.getPriority());
+
+        // # Job of main thread
         for (int i = 0; i < 10; ++i) {
             System.out.println(i + " Main Thread: " + Thread.currentThread().getName());
         }
 
+    }
+
+    static void m() {
+        System.out.println(Thread.currentThread());
     }
 }
 
 class MyRunnable implements Runnable {
 
     @Override
-    public void run() { // job of the Runnable item
+    public void run() {
+        System.out.println();
+        // # Job of Runnable item
         for (int i = 0; i < 10; ++i) {
             System.out.println(i + " MyRunnable : " + Thread.currentThread().getName());
         }
@@ -29,8 +42,9 @@ class MyThread2 extends Thread {
 
     @Override
     public void run() {
-        for (int i = 0; i < 10; ++i) {
-            System.out.println(i + " Child Thread: " + Thread.currentThread().getName());
-        }
+        System.out.println("executing MyThread2 job");
+        // for (int i = 0; i < 10; ++i) {
+        //     System.out.println(i + " Child Thread: " + Thread.currentThread().getName());
+        // }
     }
 }
