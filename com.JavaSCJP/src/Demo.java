ccc84a6bde6965a10c6fe37dbcccc888167b219e
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-11-22 21:31:16 -0500

    started multithreading all cases of extending Thread class

--- com.JavaSCJP/src/Demo.java
+++ com.JavaSCJP/src/Demo.java
@@ -4,6 +4,7 @@ public class Demo {
 
         MyThread t = new MyThread();
         t.start();
+        t.start();
 
         for (int i = 0; i < 10; ++i) {
             System.out.println(i + " Main Thread: " + Thread.currentThread().getName());
