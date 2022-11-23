896b714a04864735df7a0d10f8cddc4f6cc65c26
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-11-23 06:42:32 -0500

    declare Thread with RunnableI

--- com.JavaSCJP/src/Demo.java
+++ com.JavaSCJP/src/Demo.java
@@ -2,8 +2,9 @@ public class Demo {
 
     public static void main(String[] args) throws InterruptedException {
 
-        MyThread t = new MyThread();
-        t.start();
+        MyRunnable r = new MyRunnable();
+
+        Thread t = new Thread(r); // r is the target, where r is Runnable(MyRunnable extends Runnable)type
         t.start();
 
         for (int i = 0; i < 10; ++i) {
@@ -12,7 +13,17 @@ public class Demo {
     }
 }
 
-class MyThread extends Thread {
+class MyRunnable implements Runnable {
+
+    @Override
+    public void run() { // job of the Runnable item
+        for (int i = 0; i < 10; ++i) {
+            System.out.println(i + " MyRunnable : " + Thread.currentThread().getName());
+        }
+    }
+}
+
+class MyThread2 extends Thread {
     @Override
     public void run() {
         for (int i = 0; i < 10; ++i) {
