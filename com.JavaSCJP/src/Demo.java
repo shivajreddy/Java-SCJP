47f005659422ee662142889bc885f8bb3ce99ca2
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-11-25 17:30:08 -0500

    Thread.yield(), Thread t.join(), Thread.sleep()

--- com.JavaSCJP/src/Demo.java
+++ com.JavaSCJP/src/Demo.java
@@ -1,16 +1,20 @@
 public class Demo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         MyRunnable.mt = Thread.currentThread();
+
         MyRunnable r = new MyRunnable();
         Thread t = new Thread(r);
         t.start();
+        // t.interrupt();
 
-        // # main thread body
+        Thread.currentThread().interrupt();
+
+        // # job of main thread
         for (int i = 0; i < 10; ++i) {
-            System.out.println("Main Thread: " + Thread.currentThread());
+            Thread.sleep(2000);
+            System.out.println(Thread.currentThread() + " : " + i);
         }
-
     }
 }
 
@@ -19,16 +23,20 @@ class MyRunnable implements Runnable {
 
     @Override
     public void run() {
-        // # yield for main thread to complete
-        try {
-            mt.join();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+
+        // # job of child method
+        for (int i = 0; i < 10; ++i) {
+            System.out.println(Thread.currentThread() + " : " + i);
         }
 
-        // # task body
-        for (int i = 0; i < 10; ++i) {
-            System.out.println("Child Thread: " + Thread.currentThread());
+        System.out.println("entering sleeping state");
+
+        try {
+            Thread.sleep(2000);
+            System.out.println("finished waiting");
+        } catch (InterruptedException e) {
+            System.out.println("IE: " + e);
         }
+
     }
 }
