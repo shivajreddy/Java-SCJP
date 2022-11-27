e10b33c2ae5148253a6115b3e6085ffd6506a896
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-11-26 20:35:43 -0500

    Thread.yield(), Thread t.join(), Thread.sleep()

--- com.JavaSCJP/src/Demo.java
+++ com.JavaSCJP/src/Demo.java
@@ -1,42 +1,20 @@
 public class Demo {
-    public static void main(String[] args) throws InterruptedException {
-
-        MyRunnable.mt = Thread.currentThread();
-
-        MyRunnable r = new MyRunnable();
-        Thread t = new Thread(r);
+    public static void main(String[] args) {
+        Animator myAnim = new Animator();
+        Thread t = new Thread(myAnim);
         t.start();
-        // t.interrupt();
-
-        Thread.currentThread().interrupt();
-
-        // # job of main thread
-        for (int i = 0; i < 10; ++i) {
-            Thread.sleep(2000);
-            System.out.println(Thread.currentThread() + " : " + i);
-        }
     }
 }
 
-class MyRunnable implements Runnable {
-    static Thread mt;
+
+class Animator implements Runnable {
+    boolean animating = true;
 
     @Override
     public void run() {
-
-        // # job of child method
-        for (int i = 0; i < 10; ++i) {
-            System.out.println(Thread.currentThread() + " : " + i);
+        while (animating) {
+            System.out.println("Move apples one frame");
         }
-
-        System.out.println("entering sleeping state");
-
-        try {
-            Thread.sleep(2000);
-            System.out.println("finished waiting");
-        } catch (InterruptedException e) {
-            System.out.println("IE: " + e);
-        }
-
     }
 }
+
