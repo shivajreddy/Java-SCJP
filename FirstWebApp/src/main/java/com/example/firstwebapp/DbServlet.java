cb560a2253cec5b643ffcf7b66c2d2e345f0325c
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-11-27 19:22:44 -0500

    servletContext, and understanding it's scope

---
+++ FirstWebApp/src/main/java/com/example/firstwebapp/DbServlet.java
@@ -0,0 +1,30 @@
+package com.example.firstwebapp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class DbServlet extends HttpServlet {
+    public DbServlet() {
+        super();
+    }
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        PrintWriter out = res.getWriter();
+        out.append("served at: ").append(req.getContextPath());
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/emplyee_db", "root", "root");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
