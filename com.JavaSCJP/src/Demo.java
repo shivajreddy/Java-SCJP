52d958d450ce1ad71585fd818d9eca8b3e33ea06
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-11-28 16:27:10 -0500

    synchronized

--- com.JavaSCJP/src/Demo.java
+++ com.JavaSCJP/src/Demo.java
@@ -1,71 +1,34 @@
 public class Demo {
-    public static void main(String[] args) {
 
-        Display d = new Display();
+    public void action() throws InterruptedException {
+        Message m = new Message();
+        synchronized (this) {
+            m.wait();
+            m.notify();
+            m.notifyAll();
+        }
+    }
 
-        DisplayJob1 job1 = new DisplayJob1(d);
-        DisplayJob2 job2 = new DisplayJob2(d);
-
-        Thread t1 = new Thread(job1);
-        Thread t2 = new Thread(job2);
-
-        t1.start();
-        t2.start();
-
+    public static void main(String[] args) throws InterruptedException {
+        Demo d = new Demo();
+        d.action();
+        System.out.println("end");
     }
 }
+
+class Message {
+    String message;
+
+    public void update() {
+        this.notify();
+        this.notifyAll();
+    }
+}
+
 
 class Display {
-    public synchronized void displayN() {
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                System.out.println(e);
-            }
-        }
-    }
+    public void m() {
 
-    public synchronized void displayC() {
-        for (int i = 65; i < 75; i++) {
-            System.out.println((char) i);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                System.out.println(e);
-            }
-        }
-    }
-
-    // synchronized(Display.class)
-    // {
-    // }
-
-}
-
-class DisplayJob1 implements Runnable {
-    Display d;
-
-    public DisplayJob1(Display d) {
-        this.d = d;
-    }
-
-    @Override
-    public void run() {
-        d.displayN();
     }
 }
 
-class DisplayJob2 implements Runnable {
-    Display d;
-
-    public DisplayJob2(Display d) {
-        this.d = d;
-    }
-
-    @Override
-    public void run() {
-        d.displayC();
-    }
-}
