fa929c0877787bb4e4c0392ee5bd67fc70abf008
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-11-28 20:15:09 -0500

    synchronized

--- com.JavaSCJP/src/Demo.java
+++ com.JavaSCJP/src/Demo.java
@@ -1,34 +1,34 @@
 public class Demo {
 
-    public void action() throws InterruptedException {
-        Message m = new Message();
+    public static void main(String[] args) throws InterruptedException {
+
+        MyThread t = new MyThread();
+        t.start();
+
+        System.out.println(t.count); // dangerous
+
+        synchronized (t) {
+            System.out.println("main thread calling wait()");
+            t.wait();
+            System.out.println("main thread got the notification");
+        }
+
+        System.out.println(t.count);
+
+    }
+}
+
+class MyThread extends Thread {
+
+    long count = 0;
+
+    public void run() {
         synchronized (this) {
-            m.wait();
-            m.notify();
-            m.notifyAll();
+            for (long i = 0; i < 999999999L; i++) {
+                count += i;
+            }
+            System.out.println("child thread sending notification");
+            this.notify();
         }
     }
-
-    public static void main(String[] args) throws InterruptedException {
-        Demo d = new Demo();
-        d.action();
-        System.out.println("end");
-    }
 }
-
-class Message {
-    String message;
-
-    public void update() {
-        this.notify();
-        this.notifyAll();
-    }
-}
-
-
-class Display {
-    public void m() {
-
-    }
-}
-
