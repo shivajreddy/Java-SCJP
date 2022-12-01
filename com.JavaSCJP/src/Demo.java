11c180b83d41671c032ad13454013c002f1c7347
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-12-01 06:20:54 -0500

    dead-lock in multithreading

--- com.JavaSCJP/src/Demo.java
+++ com.JavaSCJP/src/Demo.java
@@ -1,34 +1,85 @@
+import org.jetbrains.annotations.NotNull;
+
 public class Demo {
 
     public static void main(String[] args) throws InterruptedException {
+        A a = new A();
+        B b = new B();
+        Thread1 t1 = new Thread1(a, b);
+        Thread t2 = new Thread2(a, b);
 
-        MyThread t = new MyThread();
-        t.start();
-
-        System.out.println(t.count); // dangerous
-
-        synchronized (t) {
-            System.out.println("main thread calling wait()");
-            t.wait();
-            System.out.println("main thread got the notification");
-        }
-
-        System.out.println(t.count);
+        t1.start();
+        t2.start();
 
     }
 }
 
-class MyThread extends Thread {
 
-    long count = 0;
+class Thread1 extends Thread {
+    A a;
+    B b;
+
+    public Thread1(A a, B b) {
+        // a = this.a;
+        // b = this.b;
+        this.a = a;
+        this.b = b;
+        System.out.println("Thread1 a,b :" + a + "," + b);
+    }
 
     public void run() {
-        synchronized (this) {
-            for (long i = 0; i < 999999999L; i++) {
-                count += i;
-            }
-            System.out.println("child thread sending notification");
-            this.notify();
+        a.d1(b);
+    }
+}
+
+class Thread2 extends Thread {
+    A a;
+    B b;
+
+    public Thread2(A a, B b) {
+        // a = this.a;
+        // b = this.b;
+        this.a = a;
+        this.b = b;
+        System.out.println("Thread1 a,b :" + a + "," + b);
+    }
+
+    public void run() {
+        b.d2(a);
+    }
+}
+
+
+class A {
+    synchronized void d1(B b) {
+        System.out.println("d1 meth starting");
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
         }
+        System.out.println("calling last() on b object");
+        b.last();
+        System.out.println("finished d1 on A");
+    }
+
+    synchronized void last() {
+        System.out.println("last on A");
+    }
+}
+
+class B {
+    synchronized void d2(A a) {
+        System.out.println("d2 meth starting");
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+        }
+        System.out.println("calling last() on a object");
+        a.last();
+        System.out.println("Finished d2 on B");
+    }
+
+    synchronized void last() {
+        System.out.println("last on B");
     }
 }
