00394c0cde0023df31fcc59779f74e601ee61952
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-12-17 00:27:32 -0500

    ListIterator finished

---
+++ com.JavaSCJP/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.PriorityBlockingQueue;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Queue<?> pq = new PriorityQueue<>();
+        PriorityQueue<?> priorityQueue = new PriorityQueue<>();
+
+        Queue<?> bq = new PriorityBlockingQueue<>();
+        PriorityBlockingQueue<?> priorityBlockingQueue = new PriorityBlockingQueue<>();
+        Queue<?> lbq = new LinkedBlockingQueue<>();
+        LinkedBlockingQueue<?> linkedBlockingQueue = new LinkedBlockingQueue<>();
+
+
+    }
+}
+
