9406f5b337d150435210c5cf89c4cd94f8f39bf1
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-12-16 22:56:47 -0500

    iterator finished

--- com.JavaSCJP/src/Demo.java
+++ com.JavaSCJP/src/Demo.java
@@ -1,26 +1,34 @@
+import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.Iterator;
 import java.util.Vector;
 
 public class Demo {
     public static void main(String[] args) {
 
-        Vector<Integer> vector = new Vector<>();
 
+        ArrayList<Integer> arr = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
-            vector.addElement(i);
+            arr.add(i);
         }
+        System.out.println("arr = " + arr);
 
-        System.out.println(vector);
+        // Cursor 2 : Iterator -> Universal cursor
+        Iterator<?> iterator = arr.iterator();
+        System.out.println(iterator.getClass().getName());
 
-        // We can get the Enumeration cursor from Vector using .elements()
-        Enumeration enumeration = vector.elements();
+        System.out.println(iterator);
 
-        while (enumeration.hasMoreElements()) {
-            int i = (int) enumeration.nextElement();
+        while (iterator.hasNext()) {
+            // iterator.next() returns the object at current position, and moves the cursor forward
+            int i = (int) iterator.next();
             if (i % 2 == 0) {
                 System.out.println(i);
+                iterator.remove();
             }
         }
+
+        System.out.println("Final arr =" + arr);
 
     }
 }
