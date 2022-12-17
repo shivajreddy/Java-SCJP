3091e1d3a879055b63d99faf76d128e2d7669e06
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-12-17 13:13:37 -0500

    PriorityQueue still some doubts

--- com.JavaSCJP/src/PriorityQueueDemo2.java
+++ com.JavaSCJP/src/PriorityQueueDemo2.java
@@ -5,15 +5,26 @@ public class PriorityQueueDemo2 {
 
     public static void main(String[] args) {
 
-        PriorityQueue<String> priorityQueue = new PriorityQueue<>(15, new MyComparator());
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(15, new MyComparator());
 
-        priorityQueue.offer("s");
-        priorityQueue.offer("h");
-        priorityQueue.offer("i");
-        priorityQueue.offer("v");
-        priorityQueue.offer("a");
+        priorityQueue.offer(12);
+        priorityQueue.offer(7);
+        priorityQueue.offer(10);
+        priorityQueue.offer(1);
+        priorityQueue.offer(8);
 
-        System.out.println("priorityQueue = " + priorityQueue);
+        // System.out.println("priorityQueue = " + priorityQueue);
+
+        PriorityQueue<String> pq = new PriorityQueue<>(15, new MyComparator2());
+        pq.offer("A");
+        pq.offer("Z");
+        pq.offer("L");
+        pq.offer("B");
+        pq.offer("A");
+        pq.offer("B");
+        pq.offer("B");
+
+        System.out.println("pq = " + pq);
 
     }
 }
@@ -21,11 +32,27 @@ public class PriorityQueueDemo2 {
 
 class MyComparator implements Comparator {
 
+
+    @Override
+    public int compare(Object o1, Object o2) {
+        int x = (int) o1;
+        int y = (int) o2;
+
+        if (x > y) {
+            return 1;
+        } else if (x < y) {
+            return -1;
+        }
+        return -1;
+    }
+}
+
+class MyComparator2 implements Comparator {
+
     @Override
     public int compare(Object o1, Object o2) {
         String s1 = (String) o1;
-        String s2 = o2.toString();
-        return s2.compareTo(s1);
+        String s2 = (String) o2;
+        return s1.compareTo(s2);
     }
-
 }
