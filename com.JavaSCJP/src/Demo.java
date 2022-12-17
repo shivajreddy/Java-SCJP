dc6d1f79427712031d71963aba99788595a26d25
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-12-16 23:16:56 -0500

    ListIterator finished

--- com.JavaSCJP/src/Demo.java
+++ com.JavaSCJP/src/Demo.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 import java.util.Vector;
 
 public class Demo {
@@ -11,24 +13,37 @@ public class Demo {
         for (int i = 0; i < 10; i++) {
             arr.add(i);
         }
-        System.out.println("arr = " + arr);
+        // System.out.println("arr = " + arr);
 
         // Cursor 2 : Iterator -> Universal cursor
         Iterator<?> iterator = arr.iterator();
-        System.out.println(iterator.getClass().getName());
+        // System.out.println(iterator.getClass().getName());
 
-        System.out.println(iterator);
+        // System.out.println(iterator);
 
         while (iterator.hasNext()) {
             // iterator.next() returns the object at current position, and moves the cursor forward
             int i = (int) iterator.next();
             if (i % 2 == 0) {
-                System.out.println(i);
-                iterator.remove();
+                // iterator.remove();
             }
         }
 
-        System.out.println("Final arr =" + arr);
+
+        // Cursor 3 : ListIterator
+        ListIterator<?> listIterator = arr.listIterator();
+
+        System.out.println(listIterator);
+        System.out.println(listIterator.getClass().getName());
+
+        while (listIterator.hasNext()){
+            int i = (int) listIterator.next();
+            if (i % 2 == 0){
+                System.out.println(i);
+                listIterator.remove();
+            }
+        }
+        System.out.println("Final arr = " + arr);
 
     }
 }
