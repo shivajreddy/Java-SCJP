d165a324bb2d32ef6a834a254c6a6de80ba5a935
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-12-19 17:03:34 -0500

    IdentityHashMap

--- com.JavaSCJP/src/MapDemo.java
+++ com.JavaSCJP/src/MapDemo.java
@@ -1,32 +1,36 @@
 import java.util.HashMap;
+import java.util.IdentityHashMap;
 import java.util.LinkedHashMap;
 
 public class MapDemo {
     public static void main(String[] args) {
 
-        HashMap<String, Integer> hashMap = new HashMap<>();
-        hashMap.put("s", -32);
-        hashMap.put("h", 9);
-        hashMap.put("i", 78);
-        hashMap.put("v", 23);
-        hashMap.put("a", 12);
-        System.out.println("hashMap = " + hashMap); // {a=12, s=-32, v=23, h=9, i=78}
+        HashMap hashMap = new HashMap();
 
-        LinkedHashMap<String, Integer> linkedHashMap = new LinkedHashMap<>();
-        linkedHashMap.put("s", -32);
-        linkedHashMap.put("h", 9);
-        linkedHashMap.put("i", 78);
-        linkedHashMap.put("v", 23);
-        linkedHashMap.put("a", 12);
-        System.out.println("linkedHashMap = " + linkedHashMap); // {s=-32, h=9, i=78, v=23, a=12}
+        Integer x = new Integer(10);
+        Integer y = new Integer(10);
+
+        hashMap.put(x, "shiva");
+        hashMap.put(y, "reddy");
+
+        System.out.println(hashMap); // {10=reddy}
+
+
+        IdentityHashMap identityHashMap = new IdentityHashMap();
+
+        identityHashMap.put(x, "shiva");
+        identityHashMap.put(y, "reddy");
+
+        System.out.println(identityHashMap); // {10=shiva, 10=reddy}
+
 
     }
 }
 
 
-/* LinkedMap
+/* IdentityHashMap
  *
- * underlying data structure -> HashTable + LinkedList
- * insertion order is preserved, it is based on hash code of keys
- * LinkedHashSet, LinkedHashMap are generally used to develop memory-cache applications
+ * same as HashMap except:
+ *      1. for HashMap, JVM will use .equals() i.e., content comparison
+ *      2. but for IdentityHashMap, JVM will use == i.e., reference comparison
  */
