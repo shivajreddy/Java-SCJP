66a7d2523b3ad29175a2bcf38070e8ebddba61c2
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-12-20 11:50:57 -0500

    NavigableMap(I)

--- com.JavaSCJP/src/NavigableSetDemo.java
+++ com.JavaSCJP/src/NavigableSetDemo.java
@@ -1,5 +1,5 @@
 
-/* Navigable Set
+/* NavigableSet(I)
  *
  *
  * NavigableSet(I) methods:
@@ -12,6 +12,7 @@
  *          7. descendingSet(); -> returns NavigableSet in reverse order
  */
 
+import java.util.TreeMap;
 import java.util.TreeSet;
 
 public class NavigableSetDemo {
@@ -34,6 +35,35 @@ public class NavigableSetDemo {
         System.out.println(treeSet.pollFirst());    // 1000
         System.out.println(treeSet.pollLast());     // 5000
         System.out.println(treeSet.descendingSet());    // [4000, 3000, 2000]
+
+    }
+}
+
+/* NavigableMap(I)
+ *
+ * NavigableMap(I) methods:
+ *          1. ceilingKey(key); -> lowest pair whose key is >= e
+ *          2. higherKey(e); -> lowest pair whose key is > e
+ *          3. floorKey(e);  -> highest pair whose key is <= e
+ *          4. lowerKey(e); -> highest pair whose key is < e
+ *          5. pollFirstEntry(); -> remove and return first pair
+ *          6. pollLastEntry(); -> remove and return last pair
+ *          7. descendingMap(); -> returns NavigableMap in reverse order
+ */
+
+class NavigableMapDemo {
+    public static void main(String[] args) {
+
+        TreeMap<String, String> treemap = new TreeMap<>();
+
+        treemap.put("b", "banana");
+        treemap.put("c", "cat");
+        treemap.put("a", "apple");
+        treemap.put("d", "dog");
+        treemap.put("g", "gun");
+
+        System.out.println("treemap = " + treemap);
+
 
     }
 }
